0476f6b276cc93143c1187b2cfd10430096b7ec6
subimage <subimage@6a1f77cc-a145-0410-afd8-bd7963fcc59f>
2008-04-08 07:50:37 +0000

    Fix - issue 39
    git-svn-id: http://substruct.googlecode.com/svn/trunk/plugins/substruct@53 6a1f77cc-a145-0410-afd8-bd7963fcc59f

--- app/models/country.rb
+++ app/models/country.rb
@@ -6,9 +6,8 @@ class Country < ActiveRecord::Base
   def number_of_orders
     sql  = "SELECT COUNT(*) as count "
     sql << "FROM orders "
-    sql << "INNER JOIN order_users ON order_users.id = orders.order_user_id "
     sql << "INNER JOIN order_addresses ON ( "
-    sql << "  order_addresses.country_id = #{self.id} AND order_addresses.order_user_id = order_users.id "
+    sql << "  order_addresses.country_id = #{self.id} AND order_addresses.id = orders.shipping_address_id "
     sql << ");"
     Order.count_by_sql(sql)
   end
