0c6ec8187d191fe040a5d54bb2c336a342183cd5
subimage <subimage@6a1f77cc-a145-0410-afd8-bd7963fcc59f>
2008-04-09 08:31:46 +0000

    Fix for issue 32 and updated version in prep for a new release.
    git-svn-id: http://substruct.googlecode.com/svn/trunk/plugins/substruct@71 6a1f77cc-a145-0410-afd8-bd7963fcc59f

--- lib/substruct/version.rb
+++ lib/substruct/version.rb
@@ -2,7 +2,7 @@ module Substruct
   module Version
     MAJOR  = 1
     MINOR  = 0
-    TINY   = 'a2'
+    TINY   = 'a3'
     STRING = [MAJOR, MINOR, TINY].join('.').freeze
     #TITLE  = "Noh-Varr".freeze
   end
