da0b817ccd7292e5a6871dd8e1d1f3227d8ee076
subimage <subimage@6a1f77cc-a145-0410-afd8-bd7963fcc59f>
2008-08-29 04:14:28 +0000

    Updating routes for issue 127
    git-svn-id: http://substruct.googlecode.com/svn/trunk/plugins/substruct@148 6a1f77cc-a145-0410-afd8-bd7963fcc59f

--- config/routes.rb
+++ config/routes.rb
@@ -1,51 +1,51 @@
-ActionController::Routing::Routes.draw do |map|
+# ActionController::Routing::Routes.draw do |map|
   # default
-  map.connect '',
+  connect '',
     :controller => 'content_nodes',
     :action     => 'show_by_name',
     :name       => 'home'
-  map.connect '/',
+  connect '/',
     :controller => 'content_nodes',
     :action     => 'show_by_name',
     :name       => 'home'
 
   # Default administration mapping
-  map.connect 'admin',
+  connect 'admin',
     :controller => 'admin/orders',
     :action     => 'index'
 
-  map.connect '/blog',
+  connect '/blog',
     :controller => 'content_nodes',
     :action     => 'index'
 
-  map.connect '/blog/section/:section_name',
+  connect '/blog/section/:section_name',
     :controller => 'content_nodes',
     :action     => 'list_by_section'
 
   # Static route blog content through our content_node controller
-  map.connect '/blog/:name',
+  connect '/blog/:name',
     :controller => 'content_nodes',
     :action     => 'show_by_name'
 
 
-  map.connect '/contact',
+  connect '/contact',
     :controller => 'questions',
     :action     => 'ask'
 
-  map.connect '/store/show_by_tags/*tags',
+  connect '/store/show_by_tags/*tags',
     :controller => 'store',
     :action     => 'show_by_tags'
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
-  map.connect ':controller/service.wsdl', :action => 'wsdl'
+  connect ':controller/service.wsdl', :action => 'wsdl'
 
   # Install the default route as the lowest priority.
-  map.connect ':controller/:action/:id.:format'
-  map.connect ':controller/:action/:id'
+  connect ':controller/:action/:id.:format'
+  connect ':controller/:action/:id'
 
   # For things like /about_us, etc
-  map.connect ':name',
+  connect ':name',
     :controller => 'content_nodes',
     :action     => 'show_by_name'
-end
+# end
